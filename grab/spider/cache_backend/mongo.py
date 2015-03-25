e5bdd326a875e45ac7c249b28713df3cd6071688
neveter <venick@gmail.com>
2015-03-25 15:40:55 +0300

    Fix mongo authorization

--- grab/spider/cache_backend/mongo.py
+++ grab/spider/cache_backend/mongo.py
@@ -28,7 +28,7 @@ logger = logging.getLogger('grab.spider.cache_backend.mongo')
 
 
 class CacheBackend(object):
-    def __init__(self, database, use_compression=True, spider=None):
+    def __init__(self, database, use_compression=True, spider=None, **kwargs):
         self.spider = spider
         self.db = pymongo.Connection(**kwargs)[database]
         self.use_compression = use_compression
